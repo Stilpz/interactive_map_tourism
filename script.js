3a7fa2b9bec22a89e5e831603ff1ce91102f15c9
Stilpz <stivenlopezcar21@gmail.com>
2024-05-27 14:17:05 -0500

    se modifica para agregar poner iconos en sitios turisticos

--- script.js
+++ script.js
@@ -6,22 +6,30 @@ document.addEventListener('DOMContentLoaded', function () {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    // Marcadores con descripciones y coordenadas de puntos de interés turístico
-    var markers = [
+    // Marcadores de ciudades
+    var cities = [
         {coords: [4.3369, -75.8275], description: 'La centinela del Valle', popupText: '<b>Caicedonia</b>'},
         {coords: [4.3936, -76.0742], description: 'La tierra que endulza a Colombia', popupText: '<b>Zarzal</b>'},
         {coords: [4.4122, -76.1545], description: 'Pueblo Mágico', popupText: '<b>Roldanillo</b>'},
         {coords: [4.2769, -75.9379], description: 'Pueblo Mágico, Capital cafetera y Balcón del Valle', popupText: '<b>Sevilla</b>'},
-        {coords: [4.5714, -75.8145], description: 'La puerta al Paisaje Cultural Cafetero', popupText: '<b>La Unión</b>'},
-        // Nuevos puntos de interés turístico
-        {coords: [4.3369, -75.8275], description: 'Parque Natural Regional Planes de Sonson', popupText: '<b>Parque Natural Regional Planes de Sonson</b>'}, // Cerca de Caicedonia
-        {coords: [4.3936, -76.0742], description: 'Museo Arqueológico de Zarzal', popupText: '<b>Museo Arqueológico de Zarzal</b>'}, // Cerca de Zarzal
-        {coords: [4.4122, -76.1545], description: 'Museo Rayo', popupText: '<b>Museo Rayo</b>'}, // Cerca de Roldanillo
-        {coords: [4.2769, -75.9379], description: 'Monumento a la Gaitana', popupText: '<b>Monumento a la Gaitana</b>'}, // Cerca de Sevilla
-        {coords: [4.5714, -75.8145], description: 'Mirador Salto de Aguacatal', popupText: '<b>Mirador Salto de Aguacatal</b>'} // Cerca de La Unión
+        {coords: [4.5714, -75.8145], description: 'La puerta al Paisaje Cultural Cafetero', popupText: '<b>La Unión</b>'}
     ];
 
-    markers.forEach(function(marker) {
+    cities.forEach(function(marker) {
+        L.marker(marker.coords).addTo(map)
+            .bindPopup('<b>' + marker.popupText + '</b><br>' + marker.description);
+    });
+
+    // Marcadores de sitios de interés turístico
+    var attractions = [
+        {coords: [4.3387, -75.8272], description: 'Parque Natural Regional Planes de Sonson', popupText: '<b>Parque Natural Regional Planes de Sonson</b>'}, // Cerca de Caicedonia
+        {coords: [4.3938, -76.0786], description: 'Museo Arqueológico de Zarzal', popupText: '<b>Museo Arqueológico de Zarzal</b>'}, // Cerca de Zarzal
+        {coords: [4.4078, -76.1565], description: 'Museo Rayo', popupText: '<b>Museo Rayo</b>'}, // Cerca de Roldanillo
+        {coords: [4.2783, -75.9385], description: 'Monumento a la Gaitana', popupText: '<b>Monumento a la Gaitana</b>'}, // Cerca de Sevilla
+        {coords: [4.5689, -75.8086], description: 'Mirador Salto de Aguacatal', popupText: '<b>Mirador Salto de Aguacatal</b>'} // Cerca de La Unión
+    ];
+
+    attractions.forEach(function(marker) {
         L.marker(marker.coords, {
             icon: L.icon({
                 iconUrl: 'custom-icon.png', // Ruta de la imagen del icono personalizado
